63877226add64c47de1ccb91d631dbaf94ceaf2d
hardikraval1324 <hardik1324@gmail.com>
2022-04-01 09:28:38 +0530

    Add the base UI ✅

---
+++ app/src/main/java/com/example/unifi/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.unifi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+}
