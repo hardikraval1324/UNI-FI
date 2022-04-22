f9a55660ff476c93a861fc14f6f3824dc0b87337
tej2311 <tejgandhi2013@gmail.com>
2022-04-22 10:52:03 +0530

    add; user profile xml

---
+++ app/src/main/java/com/example/uni_fikotlin/aspring_account.kt
@@ -0,0 +1,11 @@
+package com.example.uni_fikotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class aspring_account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aspring_account)
+    }
+}
