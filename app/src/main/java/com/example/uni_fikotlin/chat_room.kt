0ce98c90bdd84a26faeb97fe46c11661b6cf0449
hardikraval1324 <hardik1324@gmail.com>
2022-05-22 15:44:12 +0530

    Final commits ✨

---
+++ app/src/main/java/com/example/uni_fikotlin/chat_room.kt
@@ -0,0 +1,11 @@
+package com.example.uni_fikotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chat_room : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_room)
+    }
+}
